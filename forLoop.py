e81de074c1e48af8c870ffa1628c96b14c18a6e5
Naman Chopra <namanchopra21087@gmail.com>
2025-11-27 21:07:46 +0530

    Python while & for loop

---
+++ forLoop.py
@@ -0,0 +1,6 @@
+x=["naman","mom","sandali"]
+for i in x:
+    print(i)
+    for j in i:
+        print(j, end=" ")
+    print()
---
+++ whileLoop.py
@@ -0,0 +1,9 @@
+i=0
+while i<10:
+    print("Naman")
+    j=0
+    while j<5:
+        print("Chopra",end=" ")
+        j+=1
+    i=i+1
+    print()
